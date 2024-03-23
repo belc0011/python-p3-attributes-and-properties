b8e856c632bd847eaa302babd602ad0fa363ce04
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-23 10:04:17 -0700

    complete attributes and properties lab

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,34 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="default name", job="Legal"):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if (type(name) is str) and (1 <= len(name) <= 25):
+            new_name = name.title()
+            self._name = new_name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        found_job = False
+        for jobs in APPROVED_JOBS:
+            if (jobs.lower() == job.lower()):
+                found_job = True
+        if (found_job):
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+    
+    job = property(get_job, set_job)
+        
